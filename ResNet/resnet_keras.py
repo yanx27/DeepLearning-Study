936a5b3dddcba9bdd57f097df75f5be8249feae0
benny <35829331+yanx27@users.noreply.github.com>
2018-08-27 19:38:08 +0800

    Update resnet_keras.py

--- ResNet/resnet_keras.py
+++ ResNet/resnet_keras.py
@@ -12,7 +12,7 @@ K.set_image_data_format('channels_last')
 K.set_learning_phase(1) # fit-1 / predict-0
 
 def load_dataset():
-    os.chdir('E:\\深度学习\\吴恩达深度学习课程\\代码\\卷积神经网络\\CNN Application\\datasets')
+    os.chdir('E:\\datasets')
     train_dataset = h5py.File('train_signs.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
@@ -61,8 +61,6 @@ def identity_block(X, f, filters, stage, block):
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
-    ### START CODE HERE ###
-
     # Second component of main path (≈3 lines)
     X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
@@ -76,9 +74,8 @@ def identity_block(X, f, filters, stage, block):
     X = Add()([X, X_shortcut])
     X = Activation('relu')(X)
 
-    ### END CODE HERE ###
-
     return X
+
 def convolutional_block(X, f, filters, stage, block, s = 2):
     """
     Implementation of the convolutional block as defined in Figure 4
@@ -111,7 +108,6 @@ def convolutional_block(X, f, filters, stage, block, s = 2):
     X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
     X = Activation('relu')(X)
-
 
     # Second component of main path (≈3 lines)
     X = Conv2D(F2, (f, f), strides = (1,1), name = conv_name_base + '2b', padding = 'same', kernel_initializer = glorot_uniform(seed=0))(X)
@@ -165,13 +161,13 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6):
     X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
     X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
 
-    # Stage 3 (≈4 lines)
+    # Stage 3 
     X = convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 2)
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
 
-    # Stage 4 (≈6 lines)
+    # Stage 4 
     X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
@@ -179,7 +175,7 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6):
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
 
-    # Stage 5 (≈3 lines)
+    # Stage 5 
     X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
     X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
     X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
